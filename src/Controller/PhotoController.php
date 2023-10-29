7e30643b9b87dd164fa56b115277cb47a94e5d0a
nvheymann <nico.v.heymann@gmx.de>
2023-10-29 16:29:38 +0100

    chore: add imge show

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -11,9 +11,11 @@ class PhotoController extends AbstractController
     #[Route('/photo', name: 'app_photo')]
     public function index(): Response
     {
-        shell_exec("gphoto2 --wait-event=1s  --set-config autofocusdrive=1 --set-config eosremoterelease=5 --wait-event-and-download=2s --filename /var/www/html/raspberry-photobox/public/assets/IMG_%d-%m-%Y_%H-%M.JPG --set-config eosremoterelease=4");
+        $filename = '/var/www/html/raspberry-photobox/public/assets/IMG'.date('m-d-Y').'.JPG';
+        shell_exec("sudo gphoto2 --wait-event=1s  --set-config autofocusdrive=1 --set-config eosremoterelease=5 --wait-event-and-download=2s --filename /var/www/html/raspberry-photobox/public/assets/IMG_%d-%m-%Y_%H-%M.JPG --set-config eosremoterelease=4");
         return $this->render('photo/index.html.twig', [
             'controller_name' => 'PhotoController',
+            'file' => $filename
         ]);
     }
 }
